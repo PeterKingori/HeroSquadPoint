0294e13a3eef3169bbe65e71ea7b9ae33fd87b5b
Peter Kingori <kpndegwa@gmail.com>
2020-04-25 23:26:37 +0300

    Add a root route in App.java that will render the home page index.hbs. The index.hbs template is created and it has a form for a user to enter the name of a hero. A POST route is also added in App.java to handle the data collected using the form.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,7 @@
+import java.util.Map;
 import java.util.HashMap;
+
+import models.Hero;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -6,5 +9,19 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args){
         staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/heros/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            request.session().attribute("name", name);
+            model.put("name", name);
+            Hero newHero = new Hero(name);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
